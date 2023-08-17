a30994cdc23b487998e82195173a56fca11feb9c
Hasan Yıldırım <hasanyldrim@gmail.com>
2023-08-17 22:01:31 +0300

    Solution And Test Projects Added For Account

---
+++ Account/Tests/Quadro.Account.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Quadro.Account.Domain.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Account/Tests/Quadro.Account.Infrastructure.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Quadro.Account.Infrastructure.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
